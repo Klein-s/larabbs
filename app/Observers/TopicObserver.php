cae0cdaed33de7b22eeaaf4466064f03cec13531
Qin <994097656@qq.com>
2020-04-15 11:25:52 +0800

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,7 +2,8 @@
 
 namespace App\Observers;
 
-use App\Handlers\SlugTranslateHandler;
+use App\Handlers\SlugTranlateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -24,9 +25,15 @@ class TopicObserver
         $topic->body = clean($topic->body, 'user_topic_body');
 
         $topic->excerpt = make_excerpt($topic->body);
-
-        if (!$topic->slug){
-            $topic->slug=app(SlugTranslateHandler::class)->translate($topic->title);
+        if ( ! $topic->slug) {
+            $topic->slug = app(SlugTranlateHandler::class)->translate($topic->title);
         }
+
     }
+//    public function saved(Topic $topic){
+//
+//        if (!$topic->slug){
+//            dispatch(new TranslateSlug($topic));
+//        }
+//    }
 }
