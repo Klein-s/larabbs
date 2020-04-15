a69936df844ae1f432c54e5ef6d5d51b895934fb
Qin <994097656@qq.com>
2020-04-15 15:08:31 +0800

    删除评论

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -22,6 +22,10 @@ class Topic extends Model
         return $this->hasMany(Reply::class);
     }
 
+    public function updateReplyCount(){
+        $this->reply_count=$this->replies->count();
+        $this->save();
+    }
 
     public function scopeWithOrder($query, $order)
     {
--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -5,6 +5,7 @@ namespace App\Observers;
 use App\Handlers\SlugTranlateHandler;
 use App\Jobs\TranslateSlug;
 use App\Models\Topic;
+use mysql_xdevapi\Table;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -20,6 +21,9 @@ class TopicObserver
     {
         //
     }
+    public function deleted(Topic $topic){
+        \DB::table('replies')->where('topic_id',$topic->id)->delete();
+    }
     public function saving(Topic $topic)
     {
         $topic->body = clean($topic->body, 'user_topic_body');
@@ -30,6 +34,7 @@ class TopicObserver
         }
 
     }
+
 //    public function saved(Topic $topic){
 //
 //        if (!$topic->slug){
