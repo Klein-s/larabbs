65e70cddc81c32f1828f79a680e496e65a815cfe
Qin <994097656@qq.com>
2020-04-16 11:15:14 +0800

    侧边栏链接

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -10,7 +11,8 @@ use Illuminate\Http\Request;
 class CategoriesController extends Controller
 {
     //
-    public function show(Category $category,Request $request,Topic $topic,User $user){
+    public function show(Category $category,Request $request,Topic $topic,
+                         User $user,Link $link){
         //读取分类ID关联的话题，并按每20条分页
         $topics=$topic->WithOrder($request->order)
             ->where('category_id',$category->id)
@@ -18,6 +20,9 @@ class CategoriesController extends Controller
             ->paginate(20);
         //活跃用户列表
         $active_users=$user->getActiveUsers();
-        return view('topics.index',compact('topics','category','active_users'));
+
+        $links=$link->getAllCached();
+        return view('topics.index',compact('topics','category',
+            'active_users','links'));
     }
 }
